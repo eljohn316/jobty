be1b11e61e12abc551e908bef2b18ed9a0b453e8
El John <bongaeljohn07@gmail.com>
2026-03-31 10:24:12 +0800

    initialize CLI commands

---
+++ src/main.py
@@ -0,0 +1,39 @@
+import typer
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def list():
+    """
+    List all saved job application entries
+    """
+    print("Job applications")
+
+
+@app.command()
+def add():
+    """
+    Add a new job application entry
+    """
+    print("Add a new job application entry")
+
+
+@app.command()
+def update(job_id: str):
+    """
+    Update a job application entry
+    """
+    print(f"Update job application {job_id}")
+
+
+@app.command()
+def delete(job_id: str):
+    """
+    Delete a job application entry
+    """
+    print(f"Delete job application entry {job_id}")
+
+
+if __name__ == "__main__":
+    app()
