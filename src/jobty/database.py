84e2b3d2ff210fd427fd7f6bd265373a0bf76fe7
El John Bonga <bongaeljohn07@gmail.com>
2026-06-01 20:07:46 +0800

    fix database url

--- src/jobty/database.py
+++ src/jobty/database.py
@@ -1,9 +1,16 @@
 from contextlib import contextmanager
+from pathlib import Path
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///jobs.db"
+HOME_DIR = Path.home()
+DB_DIR = HOME_DIR / ".jobty"
+DB_PATH = DB_DIR / "jobs.db"
+
+DB_DIR.mkdir(exist_ok=True)
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
